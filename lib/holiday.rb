01dbf5dc30f718a0fe78b597d9c976e96dd7e442
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 04:47:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 end
 
 # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+# add the second argument to the memorial day array
+#
+#
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
