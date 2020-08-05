e6f232fec4f218d0651a56ee5d0c3662c2ca67cf
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 04:56:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,10 +95,14 @@ end
 
 # return an array of holiday names (as symbols) where supply lists
 # include the string "BBQ"
+#
 
 def all_holidays_with_bbq(holiday_hash)
-  
-
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, supply|
+      holiday if supply.include?("BBQ")
+    end
+  end.flatten.compact
 end
 
 
