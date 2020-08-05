b36614aa04831a8a54d6bc300b81400d48f58c4b
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 04:54:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,12 @@ end
 # etc.
 
 def all_supplies_in_holidays(holiday_hash)
-  
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+      puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
