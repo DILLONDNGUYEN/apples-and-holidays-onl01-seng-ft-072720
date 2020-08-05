2b93162125b07be9a9b72a8637727d2134465103
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-05 04:50:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ end
 # modifies the original hash by adding supplies of a new holiday to a season
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
